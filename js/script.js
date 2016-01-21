7c31fe56a4d07abcd96fd5318bcf925d81952e99
diegoingaramo <diegoingaramo@gmail.com>
2016-01-20 21:25:15 -0300

    validation on the min number of choices(2) added

--- js/script.js
+++ js/script.js
@@ -12,16 +12,29 @@ var pollController = function pollController($scope){
     $scope.newChoices.push({placeholder: "Cleveland Cavaliers", question: ""});
     $scope.newChoices.push({placeholder: "San Antonio Spurs", question: ""});
     
+    $scope.showMaxSizeError = false;
+    
     $scope.addChoice = function () {
+        
+        if ($scope.newChoices.length >= 2)
+            $scope.showMaxSizeError = false;
+        
           $scope.newChoices.push({placeholder: "Please enter the new choice", question: ""});
     };
     
     $scope.deleteChoice = function ( choiceID ) {
         
-        $scope.newChoices.splice(choiceID, 1);
-
-  
+        if ($scope.newChoices.length <= 2)
+            $scope.showMaxSizeError = true;
+            
+        if ($scope.newChoices.length > 2)
+            $scope.newChoices.splice(choiceID, 1);
+        
     };
+    
+    $scope.hideMessage = function(){
+        $scope.showMaxSizeError = false;
+    }
 
 };
 
