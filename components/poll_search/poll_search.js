3c8a7fc90b3b041d26a119eb32a23b7322ee99a8
diegoingaramo <diegoingaramo@gmail.com>
2016-01-22 23:01:46 -0300

    search polls page added

--- components/poll_search/poll_search.js
+++ components/poll_search/poll_search.js
@@ -1,4 +1,31 @@
-angular.module('poll.search', ['ngNewRouter'])
-  .controller('PollSearchController', ['$routeParams', function ($scope, $routeParams) {
-    //this.name = 'Friend';
-  }]);
+/* Service */
+
+var pollSearchService = function($http) {
+
+    var search = function() {
+      return [{id: 1, question: 'Do you like coke ?'}, {id: 2, question: 'Do you like pepsi ?'},
+             {id: 3, question: "What's your favorite movie ?"}];
+    };
+    
+   
+    return {
+      search: search
+    };
+    
+ };
+
+/* Controller */
+
+var pollSearchController = function ($scope, $routeParams, pollSearchService) {
+      
+    $scope.searchText = "";
+    $scope.searchResults = [];
+    
+    $scope.search = function(){
+        $scope.searchResults = pollSearchService.search($scope.searchText);
+    };
+      
+};
+
+angular.module('poll.search', ['ngNewRouter']).factory('pollSearchService', ['$http', pollSearchService])
+  .controller('PollSearchController', ['$scope', '$routeParams','pollSearchService', pollSearchController]);
