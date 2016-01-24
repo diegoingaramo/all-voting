2d55b1d5053a5b2cdfb95ac3eb0df243248decb5
diegoingaramo <diegoingaramo@gmail.com>
2016-01-24 20:33:28 -0300

    google chart added

--- components/poll_search/poll_search.js
+++ components/poll_search/poll_search.js
@@ -26,6 +26,7 @@ var pollSearchController = function ($scope, $routeParams, pollSearchService) {
       
     $scope.searchText = "";
     $scope.searchResults = [];
+    $scope.viewTable = true;
     $scope.selectedPoll = {};
     $scope.selectedPoll.question = "";
     $scope.selectedPoll.options = [];
@@ -37,8 +38,44 @@ var pollSearchController = function ($scope, $routeParams, pollSearchService) {
     $scope.viewPollResult = function(searchResult){
         $scope.selectedPoll.question = searchResult.question;
         $scope.selectedPoll.options = pollSearchService.getOptionsByPoll(searchResult.id);
+        $scope.viewTable = true;
         $("#pollres-container").modal();
     };
+    
+    $scope.viewPollChart = function(){
+        
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'option');
+        data.addColumn('number', '#votes');
+        var dataRows = [];
+        
+        $scope.selectedPoll.options.forEach(function(element, index, array){
+            dataRows.push([element.text,element.votes]);
+        });
+        
+        data.addRows(dataRows);
+
+        // Set chart options
+        var options = {is3D: false,
+                       width: 800, 
+                       height: 300,
+                       titlePosition: 'none',
+                       'chartArea': {'width': '100%', 'height': '70%'}
+                      };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+        
+        $scope.viewTable = false;
+        
+    }
+    
+    $scope.viewPollTable = function(){
+        $scope.viewTable = true;
+    };
+    
       
 };
 
