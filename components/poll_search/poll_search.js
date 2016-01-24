8e569650a693bbdf126d59546e37ceefdc823567
diegoingaramo <diegoingaramo@gmail.com>
2016-01-24 12:14:37 -0300

    Poll result table added

--- components/poll_search/poll_search.js
+++ components/poll_search/poll_search.js
@@ -2,14 +2,20 @@
 
 var pollSearchService = function($http) {
 
-    var search = function() {
+    var search = function(searchText) {
       return [{id: 1, question: 'Do you like coke ?'}, {id: 2, question: 'Do you like pepsi ?'},
              {id: 3, question: "What's your favorite movie ?"}];
     };
     
+    var getOptionsByPoll = function(pollID) {
+      return [{id: 2, text: 'Yes', votes: 124}, {id: 5, text: 'No', votes: 87},
+             {id: 9, text: "I don't know", votes: 23}];
+    };
+    
    
     return {
-      search: search
+      search: search,
+      getOptionsByPoll: getOptionsByPoll  
     };
     
  };
@@ -20,9 +26,18 @@ var pollSearchController = function ($scope, $routeParams, pollSearchService) {
       
     $scope.searchText = "";
     $scope.searchResults = [];
+    $scope.selectedPoll = {};
+    $scope.selectedPoll.question = "";
+    $scope.selectedPoll.options = [];
     
     $scope.search = function(){
         $scope.searchResults = pollSearchService.search($scope.searchText);
+    };
+    
+    $scope.viewPollResult = function(searchResult){
+        $scope.selectedPoll.question = searchResult.question;
+        $scope.selectedPoll.options = pollSearchService.getOptionsByPoll(searchResult.id);
+        $("#pollres-container").modal();
     };
       
 };
