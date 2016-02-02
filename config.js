d1f4b6621cda74b96386d7adb12a11a11b0cafbd
diegoingaramo <diegoingaramo@gmail.com>
2016-02-02 10:34:02 -0300

    mongodb local configuration added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'allvoting',
-    'database': process.env.MONGOLAB_URI
+    'database': process.env.MONGOLAB_URI || 'mongodb://allvoting:allvoting@127.0.0.1:27017/all-voting'
 
 };
